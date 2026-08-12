e52421fcd95aa7b627f13c9716995ce525b9b37d
Andranu Dhawy Purditya <andranu.dhawy@ui.ac.id>
2026-08-12 21:22:26 +0700

    adding log in and log out option for user

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_chat_apps/screens/auth_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mobile_chat_apps/screens/chat_screen.dart';
+import 'package:mobile_chat_apps/screens/splash_screen.dart';
 import 'firebase_options.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
@@ -22,7 +26,18 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(), 
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return ChatScreen();
+          } 
+          return AuthScreen();
+        }
+      )
     );
   }
 }
