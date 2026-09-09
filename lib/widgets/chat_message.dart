f25546a525a9242b00dba0e52d4c9fb98e05b518
adpsxx <ranupurditya@gmail.com>
2026-09-09 22:01:33 +0700

    Changing Name

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,13 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_chat_apps/widgets/message_bubble.dart';
 
-class ChatMessage extends StatelessWidget {
+class ChatMessage extends StatefulWidget {
   const ChatMessage({super.key});
 
-  
-  
+  @override
+  State<ChatMessage> createState() => _ChatMessageState();
+}
+
+class _ChatMessageState extends State<ChatMessage> {
+
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+    final token = await fcm.getToken();
+
+    fcm.subscribeToTopic("chat");
+  }
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotification();
+  }
+
   @override
   Widget build(BuildContext context) {
     final authenticatedUser = FirebaseAuth.instance.currentUser!;
